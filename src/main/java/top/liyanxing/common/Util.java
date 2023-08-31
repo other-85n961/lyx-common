bbef89f7d1e2b94c1f1739e2504c449470b99576
李艳兴 <liyanxingelon@gmail.com>
2023-08-31 14:33:18 +0800

    feat: 添加判断是不是图片方法

--- src/main/java/top/liyanxing/common/Util.java
+++ src/main/java/top/liyanxing/common/Util.java
@@ -33,4 +33,29 @@ public class Util
         String imgTypeStr = String.join(StrUtil.COMMA, Constants.ImgType.LIST);
         return StrUtil.containsIgnoreCase(imgTypeStr, type);
     }
+
+    /**
+     * 判断是不是视频
+     * @param filePath 文件
+     * @return 是不是
+     */
+    public static boolean isVideo(String filePath)
+    {
+        return isVideo(FileUtil.file(filePath));
+    }
+
+    /**
+     * 判断是不是视频
+     * @param file 文件
+     * @return 是不是
+     */
+    public static boolean isVideo(File file)
+    {
+        Assert.notNull(file, "文件不能为null");
+        Assert.isTrue(FileUtil.exist(file), "文件不存在");
+        Assert.isTrue(FileUtil.isFile(file), "不是文件");
+        String type = FileUtil.getType(file);
+        String videoTypeStr = String.join(StrUtil.COMMA, Constants.VideoType.LIST);
+        return StrUtil.containsIgnoreCase(videoTypeStr, type);
+    }
 }
